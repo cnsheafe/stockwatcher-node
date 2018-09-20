fa7ccb8bcff43a855c38d5903cb71bcc88d5b5fe
Chris <cnsheafe@gmail.com>
2018-09-19 19:19:54 -0600

    Use base Db for symbolDb

--- src/db/symbol.ts
+++ src/db/symbol.ts
@@ -1,12 +1,6 @@
-import { Pool } from 'pg'
+import Db from './db'
 
-export class SymbolDb {
-  constructor (pool: Pool) {
-    this.pool = pool
-  }
-
-  private pool: Pool
-
+export class SymbolDb extends Db {
   async getOneById (id: number) {
     const { rows } = await this.pool.query('SELECT * FROM Symbols where id=$1', [id])
 
