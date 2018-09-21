628340cf2bb185a8511982a819aa2161882c8004
Chris <cnsheafe@gmail.com>
2018-09-21 12:05:50 -0600

    Resolve fetch on request close event, not response

--- src/lib/lib.fetch.ts
+++ src/lib/lib.fetch.ts
@@ -7,21 +7,25 @@ interface IFetchResponse {
 
 const fetch = async (options: RequestOptions, body?: string) => {
   return new Promise<IFetchResponse>((resolve, reject) => {
+    let data = ''
+    let status = 500
     const req = request(options, (res) => {
-      let data = ''
       res
         .setEncoding('utf8')
         .on('data', (d) => {
-          data += d
+          data = data.concat(d)
         })
         .on('error', (err) => reject({ err, status: res.statusCode }))
-        .on('close', () => resolve({ data, status: res.statusCode }))
+        .on('end', () => { status = res.statusCode })
     })
 
     if (options.method !== 'GET') req.write(body)
 
-    req.on('error', (err) => reject({ err }))
-    req.end()
+    req.on('error', (err) => reject(err))
+      .on('close', () => {
+        resolve({ data, status })
+      })
+      .end()
   })
 }
 
